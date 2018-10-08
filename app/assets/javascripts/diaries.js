9220e6afebd3659a0e7be0d36ae5e80b824dbdc1
umeneri <koishimst@gmail.com>
2018-10-08 10:59:28 +0900

    javascript liff test

--- app/assets/javascripts/diaries.js
+++ app/assets/javascripts/diaries.js
@@ -1,8 +1,24 @@
 function test() {
   console.log('test');
-}
+  console.log(liff);
 
-test();
+  liff.getProfile().then(function (profile) {
+    window.alert(profile);
+
+    liff.sendMessages([
+      {
+        type: 'text',
+        text: 'From:' + profile.displayName
+      }
+    ]).then(function () {
+      liff.closeWindow();
+    }).catch(function (error) {
+      window.alert('Error sending message: ' + error.message);
+    });
+  }).catch(function (error) {
+    window.alert("Error getting profile: " + error.message);
+  });
+}
 
 $(window).on('load', function(){
   liff.init(function (data) {
@@ -11,5 +27,27 @@ $(window).on('load', function(){
   }, function(error) {
     window.alert(error);
   });
+
+  initCalendar();
 });
+
+
+function initCalendar() {
+  var element = document.getElementById("my-calendar");
+  var myCalendar = new jsCalendar();
+  myCalendar._construct([element]);
+  console.log(myCalendar);
+
+  var inputA = document.getElementById("my-input-a");
+  var inputB = document.getElementById("my-input-b");
+
+  myCalendar.onDateClick(function(event, date){
+    inputA.value = date.toString();
+  });
+
+  myCalendar.onMonthChange(function(event, date){
+    inputB.value = date.toString();
+    console.log(date.getMonth());
+  });
+};
 
--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -61,6 +61,10 @@ class DiariesController < ApplicationController
     end
   end
 
+  def calendar
+    @diaries = Diary.all
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_diary
