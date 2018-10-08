c18a78222031eb3660f56f0d9c333d8c95343e1e
umeneri <koishimst@gmail.com>
2018-10-08 11:16:36 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :diaries
-
-  get 'calendar', to: 'diaries#calendar'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :diaries do
+    collection do
+      get 'calendar', to: 'diaries#calendar'
+    end
+  end
 end
